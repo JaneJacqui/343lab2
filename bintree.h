187b085bc916a1855b6fd15af8256f77060120e5
lisapeterman <lisapeterman88@gmail.com>
2014-01-24 16:13:49 -0800

    Update bintree.h

--- bintree.h
+++ bintree.h
@@ -16,40 +16,40 @@ using namespace std;
 //
 // Assumptions:
 //   - Arrays used when converting between array and tree format are built at
-//     a stratic length of 100 elements
+//     a static length of 100 elements
 //-----------------------------------------------------------------------------
 
 
 class BinTree { // you add class/method comments and assumptions 
- 
+
 	friend ostream& operator<<(ostream &, BinTree &);
 
-public: 
+public:
 	BinTree(); // constructor 
 	BinTree(const BinTree &); // copy constructor 
 	~BinTree(); // destructor, calls makeEmpty 
 	bool isEmpty() const; // true if tree is empty, otherwise false 
 	void makeEmpty(); // make the tree empty so isEmpty returns true 
-	BinTree& operator=(const BinTree &); 
-	bool operator==(const BinTree &) const; 
-	bool operator!=(const BinTree &) const; 
-	bool insert(NodeData*); 
-	bool retrieve(const NodeData, NodeData*&) const; 
+	BinTree& operator=(const BinTree &);
+	bool operator==(const BinTree &) const;
+	bool operator!=(const BinTree &) const;
+	bool insert(NodeData*);
+	bool retrieve(const NodeData, NodeData*&) const;
 	int getHeight(const NodeData &) const;
-	void bstreeToArray(NodeData* []); // fills an array of NodeData*
-	void arrayToBSTree(NodeData* []); // builds a balanced tree from a sorted array
+	void bstreeToArray(NodeData*[]);
+	void arrayToBSTree(NodeData*[]); // builds a balanced tree from a sorted array
 	void displaySideways() const; // provided, displays the tree sideways 
-//	ostream& dumpInorder(ostream&);
+	ostream& publicHelper(ostream &);
 
-private: 
+private:
 
-	struct Node { 
+	struct Node {
 		NodeData* data; // pointer to data object 
 		Node* left; // left subtree pointer
 		Node* right; // right subtree pointer
 
 		friend bool operator==(Node const & lhs, Node const & rhs) {
-			return lhs.data == rhs.data 
+			return lhs.data == rhs.data
 				&& ((lhs.left == 0 && rhs.left == 0)
 				|| (lhs.left != 0 && rhs.left != 0 && *lhs.left == *rhs.left))
 				&& ((lhs.right == 0 && rhs.right == 0)
@@ -61,13 +61,17 @@ private:
 			delete right;
 		}
 
-	}; 
+	};
 
 	Node* root; // root of the tree 
- 
-	// utility functions 
-//	ostream& outputHelper(ostream&, Node*);
-	void inorderHelper() const; 
-	void sideways(Node*, int) const; // provided, helper for displaySideways() 
 
-}; 
+	// utility functions 
+	//	ostream& outputHelper(ostream&, Node*);
+	void sideways(Node*, int) const; // provided, helper for displaySideways() 
+	int inorderHelper(Node*, NodeData*[], int) const;
+	int heightHelper(const NodeData&, const Node*, int) const;
+	Node* treeHelper(NodeData* [], int, int, Node*);
+	void copyHelper(Node*, const BinTree&); // copies a tree
+	ostream& outputHelper(ostream&, Node*);
+
+};
